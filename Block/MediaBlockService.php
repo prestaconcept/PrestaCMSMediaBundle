ece43bc3c354e6f9c69153a3f3a09e07737cf82d
Nicolas Bastien <nbastien@prestaconcept.net>
2013-03-06 17:26:54 +0100

    Add format in media block

--- Block/MediaBlockService.php
+++ Block/MediaBlockService.php
@@ -10,6 +10,8 @@
 namespace Presta\CMSMediaBundle\Block;
 
 use Presta\CMSCoreBundle\Block\BaseModelBlockService;
+use Sonata\BlockBundle\Model\BlockInterface;
+use Sonata\AdminBundle\Form\FormMapper;
 
 /**
  * Media Block use SonataMediaBundle
@@ -34,12 +36,56 @@ class MediaBlockService extends BaseModelBlockService
     }
 
     /**
+     * @return mixed
+     */
+    public function getMediaPool()
+    {
+        return $this->getModelAdmin('sonata.media.admin.media')->getPool();
+    }
+
+    /**
+     * Returns available formats
+     *
+     * @param string $context
+     * @return array
+     */
+    protected function getFormatChoices($context = 'prestacms')
+    {
+        $formatChoices = array();
+
+        $formats = $this->getMediaPool()->getFormatNamesByContext($context);
+
+        foreach ($formats as $code => $format) {
+            $formatChoices[$code] = $this->trans('media.format.' . $code);
+        }
+
+        return $formatChoices;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getFormSettings(FormMapper $formMapper, BlockInterface $block)
+    {
+        $formatChoices = $this->getFormatChoices();
+
+        return array_merge(
+            array(
+                array('format', 'choice', array('required' => count($formatChoices) > 0, 'choices' => $formatChoices))
+            )
+          ,  parent::getFormSettings($formMapper, $block)
+        );
+    }
+
+
+    /**
      * {@inheritdoc}
      */
     public function getDefaultSettings()
     {
         return array(
-            'media' => null
+            'media' => null,
+            'format' => 'prestacms_gallery_horizontal_thumb'
         );
     }
 }
